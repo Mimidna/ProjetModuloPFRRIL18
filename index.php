155cf9504ee4ff64d10e93d5cff59cc37b90a436
David <david.richomme@viacesi.fr>
2020-01-20 16:41:48 +0100

    ajout PHP de connexion

--- index.php
+++ index.php
@@ -76,14 +76,54 @@
                         
                         <button type="submit" class="btn btn-primary" id="connexion">
                              <?php  
-
-		  
-		                        if ($reponse->fetch()) {
-			                        echo 'Connexion OK';
-
+                                    session_start();
+                                    if(isset($_POST['name']) && isset($_POST['mdp']))
+                                    {
+                                       // connexion à la base de données
+                                        $db_username = 'root';
+                                        $db_password = '';
+                                        $db_name     = 'modulo';
+                                        $db_host     = 'localhost';
+                                        $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
+                                               or die('could not connect to database');
+                                        // on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars
+                                        // pour éliminer toute attaque de type injection SQL et XSS
+                                        $username = mysqli_real_escape_string($db,htmlspecialchars($_POST['name'])); 
+                                        $password = mysqli_real_escape_string($db,htmlspecialchars($_POST['mdp']));
+                                      
+                                        if($username !== "" && $password !== "")
+                                        {
+                                            $requete = "SELECT count(*) FROM commercial where 
+                                                  commercial_identifiant = '".$username."' and commercial_motDePasse = '".$password."' ";
+                                            $exec_requete = mysqli_query($db,$requete);
+                                            $reponse      = mysqli_fetch_array($exec_requete);
+                                            $count = $reponse['count(*)'];
+                                            if($count!=0) // nom d'utilisateur et mot de passe corrects
+                                            {
+                                             echo 'Aucune erreur détéctée !!  ';
+                                               $_SESSION['name'] = $username;
+                                               header('Location: espacePerso.php');
+                                            }
+                                            else
+                                            {
+                                             echo 'Erreur 1 !!  ';
+                                               header('Location: index.php?erreur=1'); // utilisateur ou mot de passe incorrect
+                                            }
+                                        }
+                                        else
+                                        {
+                                          echo 'Erreur 2 !!  ';
+                                           header('Location: index.php?erreur=2'); // utilisateur ou mot de passe vide
+                                        }
+                                    }
+                                    else
+                                    {
+                                       header('Location: index.php');
+                                    }
+                                    mysqli_close($db); // fermer la connexion
 		                        }
 				                ?>
-                             <a href="pages/espacePerso.php">Connexion		</a>
+                             <a href="pages/espacePerso.php">Connexion</a>
                         </button>
 
                         <!-- si la connexion est valide par le mot de passe et l'identifiant l'utilisateur se retrouve a la page index.php sinon un message d'erreur s'affiche informant de qu'il est irroné
